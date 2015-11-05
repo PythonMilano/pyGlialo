d7aeded99c6a940d423113850126ccd242e110db
Zanza00 <zanza00@gmail.com>
2015-11-05 13:08:52 +0100

    implemented the random part

--- pyGlialo.py
+++ pyGlialo.py
@@ -2,6 +2,7 @@ import json
 import urllib.error
 import urllib.parse
 import urllib.request
+from random import randint
 
 from secrets import meetup_api_key
 
@@ -9,6 +10,10 @@ from secrets import meetup_api_key
 def get_event_id():
     # TODO recuperare l'id da https://api.meetup.com/2/events? -> result[0].ID
     return "226276326"
+
+def spin_the_wheel(data):
+    max_int = len(data['results'])
+    return randint(0, max_int)
 
 
 event_id = get_event_id()
@@ -18,4 +23,7 @@ url = "https://api.meetup.com/2/rsvps?offset=0&format=json&event_id=" + \
       meetup_api_key
 response = urllib.request.urlopen(url)
 data = json.loads(response.read().decode("utf-8"))
-print(data)
+
+lucky_number = spin_the_wheel(data)
+
+print(data['results'][lucky_number]['member']['name'])
