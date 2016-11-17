910d14bea7c0e420a1f54f3ebf7cc8db00ccd1bb
Christian Strappazzon <christian.strappazzon@indicod-ecr.it>
2016-11-17 10:18:14 +0100

    config url v3 meetup api

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ except ImportError:
     meetup_api_key = 'no-api-key-provided'
 
 URL_EVENTS = 'https://api.meetup.com/Python-Milano/events'
-URL_RSVPS = 'https://api.meetup.com/2/rsvps?offset=0&format=json&event_id={id}&key=' + meetup_api_key
+URL_RSVPS = 'https://api.meetup.com/Python-Milano/events/{id}/rsvps/?key=' + meetup_api_key
