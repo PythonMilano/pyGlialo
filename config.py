010be9a34313ff0749125bd88a55805004207e11
Christian Strappazzon <christian.strappazzon@indicod-ecr.it>
2017-03-25 21:43:43 +0100

    Removed api key: not required

--- config.py
+++ config.py
@@ -1,7 +1,2 @@
-try:
-    from secrets import meetup_api_key
-except ImportError:
-    meetup_api_key = 'no-api-key-provided'
-
 URL_EVENTS = 'https://api.meetup.com/Python-Milano/events'
-URL_RSVPS = 'https://api.meetup.com/Python-Milano/events/{id}/rsvps/?key=' + meetup_api_key
+URL_RSVPS = 'https://api.meetup.com/Python-Milano/events/{id}/rsvps/'
